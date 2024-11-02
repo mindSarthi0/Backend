56bc8fc1c8a814559242ced3992961a6aa0b2706
Ashwani Arya <ashwaniparker@gmail.com>
2024-11-02 17:18:21 +0530

    fix: added try email on failed

--- API/mail.go
+++ API/mail.go
@@ -2,9 +2,10 @@ package API
 
 import (
 	"fmt"
-	"log"
-
 	"gopkg.in/mail.v2" // Sending email
+	"log"
+	"net"
+	"time"
 )
 
 func sendEmail(to string, subject string, body string, attachmentPath string) {
@@ -22,11 +23,39 @@ func sendEmail(to string, subject string, body string, attachmentPath string) {
 	// Configure Zoho SMTP settings
 	d := mail.NewDialer("smtp.zoho.in", 587, "cognify@duinvites.com", "duM7zATwBkKd") //
 
-	if err := d.DialAndSend(m); err != nil {
-		log.Fatal(err)
-	}
+	sendEmailWithRetry(m, 3, d)
 
 	log.Println("Email sent successfully!")
+}
+
+func sendEmailWithRetry(m *mail.Message, retries int, d *mail.Dialer) {
+	for attempt := 1; attempt <= retries; attempt++ {
+		err := d.DialAndSend(m)
+		if err == nil {
+			log.Printf("Email sent successfully on attempt %d", attempt)
+			return
+		}
+
+		log.Printf("Attempt %d: Error sending email: %v", attempt, err)
+		if netErr, ok := err.(net.Error); ok && netErr.Temporary() {
+			log.Println("Temporary error, retrying...")
+			time.Sleep(2 * time.Second)
+			continue
+		}
+		logDetailedError(err)
+		return
+	}
+	log.Println("Failed to send email after retries")
+}
+
+func logDetailedError(err error) {
+	log.Printf("Error sending email: %v", err)
+
+	// Check if it's a network error to get more specific information
+	if netErr, ok := err.(net.Error); ok {
+		log.Printf("Is temporary: %v", netErr.Temporary())
+		log.Printf("Is timeout: %v", netErr.Timeout())
+	}
 }
 
 func SendBIG5Report(to string, name string, attachmentPath string) {
